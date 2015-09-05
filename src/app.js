e1c178b81ec2c57fe19b1cd7287b73440e6031aa
ryotamac <tennis.suta6@gmail.com>
2015-09-06 02:35:53 +0900

    first

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import UserList from "./components/UserList.js"
+
+React.render(
+  <UserList />,
+  document.getElementById("root")
+)
---
+++ src/components/UserList.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ryota on 15/09/06.
+ */
+import React from "react";
+
+class UserList extends React.Component{
+  constructor(props,content) {
+    super(props, content);
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
